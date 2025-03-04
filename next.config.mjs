370f967753bc22bfebc7c85fe488d0f2315c96e2
petvices <petvices@gmail.com>
2025-03-04 13:30:32 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'softwans'; // Reemplaza con el nombre de tu repositorio
 
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: '', 
+  assetPrefix: '', 
   images: {
-    unoptimized: true, // Para evitar problemas con imágenes en `next export`
+    unoptimized: true,
   },
 };
 
