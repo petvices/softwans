ec5cdb3079fbc0647dc143e293b06934b3e93ce8
wanxiturro <lbpf2021jaca@gmail.com>
2025-04-02 11:43:04 -0400

    Update calendar.tsx

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -55,9 +55,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-      }}
+        IconLeft: (_props: React.ComponentProps<"svg">) => <ChevronLeft className="h-4 w-4" />,
+        IconRight: (_props: React.ComponentProps<"svg">) => <ChevronRight className="h-4 w-4" />,
+      }}      
       {...props}
     />
   )
