05ba644fc4bbf4b6cb00c5bf6374ad6ed5d6c419
wanxiturro <lbpf2021jaca@gmail.com>
2025-04-03 15:13:21 -0400

    Update to Softwans

--- components/softwans/portfolio-section.tsx
+++ components/softwans/portfolio-section.tsx
@@ -9,7 +9,7 @@ import { ArrowRight } from "lucide-react"
 export function PortfolioSection() {
   const projects = [
     {
-      title: "Landing Page",
+      title: "Alimentos Satvicos",
       category: "Desarrollo Web",
       image: "https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=2015&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       link: "https://satvicosalimentos.com/",
