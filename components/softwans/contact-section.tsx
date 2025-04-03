ebcfd0f163b69cddf175579aba5ba6577551369f
wanxiturro <lbpf2021jaca@gmail.com>
2025-04-03 09:44:16 -0400

    Update contact-section.tsx

--- components/softwans/contact-section.tsx
+++ components/softwans/contact-section.tsx
@@ -7,7 +7,6 @@ import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Label } from "@/components/ui/label"
-import { Check, Mail, MapPin, MessageSquare, Phone } from "lucide-react"
 
 export function ContactSection() {
   const [formData, setFormData] = useState({
