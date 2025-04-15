42f3a7042c52b0ad752cebd732ee54fbe6c06f5d
wanxiturro <lbpf2021jaca@gmail.com>
2025-04-15 11:13:03 -0400

    Update page.tsx

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import Image from "next/image"
 import Link from "next/link"
 import type { Metadata } from "next"
-import { Instagram, Globe, ArrowLeft } from "lucide-react"
+import { Instagram, WhatsApp, Globe, ArrowLeft } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { WhatsAppButton } from "@/components/softwans/whatsapp-button"
 
@@ -50,7 +50,7 @@ export default function LinksPage() {
 
           <LinkButton
             href="https://wa.me/message/Y5IO4FCCEMICH1"
-            icon={<WhatsAppButton className="h-5 w-5" />}
+            icon={<WhatsApp className="h-5 w-5" />}
             label="WhatsApp"
             color="bg-[#25D366]"
           />
