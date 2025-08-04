7d37a1e0253c0fe9194f9bfafa61350c9d632d1f
wanxiturro <lbpf2021jaca@gmail.com>
2025-08-04 10:11:03 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,25 +40,6 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-        {process.env.NODE_ENV === 'production' && (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function loadScript(a){
-                  var b = document.getElementsByTagName("head")[0],
-                      c = document.createElement("script");
-                  c.type = "text/javascript";
-                  c.src = "https://tracker.metricool.com/resources/be.js";
-                  c.onreadystatechange = a;
-                  c.onload = a;
-                  b.appendChild(c);
-                })(function(){
-                  beTracker.t({hash: "e6b053fe2f9c462a7cfc05ef6427b24d"});
-                });
-              `
-            }}
-          />
-        )}
         <link rel="icon" href="/favicon.ico" />
         {/* Opcional: otros favicons */}
         <link rel="icon" href="images/favicon.ico" />
